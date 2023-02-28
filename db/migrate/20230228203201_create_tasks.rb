ede5c8baa15e66d5a93b00f5f7e07ac16ec47c14
Tracy Mumbi <tracy.mumbi@student.moringaschool.com>
2023-03-01 00:10:27 +0300

    bundle install, did the migration and created users table and the tasks table

---
+++ db/migrate/20230228203201_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :description
+      t.string :completion_status
+      t.date :due_date
+      t.integer :user_id
+    end
+  end
+end
