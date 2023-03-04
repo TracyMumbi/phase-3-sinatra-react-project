7258589c01d9b9df5125776d00eedb3cca83acd4
Tracy Mumbi <tracy.mumbi@student.moringaschool.com>
2023-03-04 11:40:37 +0300

    Did seeding for the users

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,21 +10,6 @@ puts "🌱 Seeding spices..."
       password: "password"
     )
   end
-  # run a loop 50 times
-  50.times do |i|
 
-        # start_date = Date.new(2023, 1, 1)
-        # end_date = Date.new(2023, 12, 31)
-
-        # random_date = Faker::Date.between(start_date: start_date, end_date: end_date)
-        Task.create(
-          user_id: rand(1..10),
-          name: Faker::Name.name,
-          description: Faker::Lorem.sentence,
-          date: Faker::Date.between(from: '2023-03-03', to: '2023-03-05'),     
-          due_date: Faker::Date.between(from: '2023-01-01', to: '2023-03-31'),
-          completion_status: ['In progress', 'Completed', 'On hold'].sample,
-        )
-      end
 
 puts "✅ Done seeding!"
